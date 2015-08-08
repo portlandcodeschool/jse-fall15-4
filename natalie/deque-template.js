dcc9d2af6258544428259e494e936e28beb4a0a7
natalieogle <natalie.ogle85@gmail.com>
2015-08-07 19:49:11 -0700

    copied files to personal folder and finished number 1.

---
+++ natalie/deque-template.js
@@ -0,0 +1,64 @@
+// Problem 2a): build a deque factory
+//-------
+
+// The factory itself:
+function makeDeque(values) {
+	// ...
+	// return an instance object which has the following methods:
+	// length
+	// top
+	// bottom
+	// pop
+	// push
+	// shift
+	// unshift
+	// cut
+	// map
+	// sort
+
+}
+
+// The factory's instance methods:
+makeDeque.arrLength = function() { 	// can't use property named length;
+				// as a function, makeDeque has a predefined length property.
+	//...
+}
+
+makeDeque.top = function() {
+	//...
+}
+
+makeDeque.bottom = function() {
+	//...
+}
+
+makeDeque.pop = function() {
+	//...
+}
+
+makeDeque.push = function(val) {
+	//...
+}
+
+makeDeque.shift = function() {
+	//...
+}
+
+makeDeque.unshift = function(val) {
+	//...
+}
+
+makeDeque.cut = function() {
+	//...
+}
+
+makeDeque.map = function(convertValFn) {
+	//...
+}
+
+makeDeque.sort = function(compareValsFn) {
+	//...
+}
+
+// Feel free to write tests for your code!
+
