793122a52f981563a0b75a87d279bfd07e968c44
sarah <sarahkelleher234@gmail.com>
2015-08-10 17:51:42 -0700

    homework

---
+++ sarah/homework.js
@@ -0,0 +1,126 @@
+hmwk.js
+
+
+
+function makeCard(id) {
+	if (id < 0) {
+		return null;
+	};
+	else (id > 52) {
+		return null;
+	};
+	
+	Var card =
+	}
+	id: id,
+	rank: makeCard.rank,
+	suit: makeCard.suit,
+	color: makeCard.color,
+	name: makeCard.cardName,
+	};
+
+	var rank = {A,1, 2, 3, 4,...52}
+	makeCard.rank = function(this.rank){
+		return this in rank
+	};
+
+	var suit = {spade, club, diamond, heart}
+	makeCard.suit = function(this.suit) {
+		return this in suit
+	};
+
+	var color = {black, red}
+	makeCard.color = function(this.color) {
+		return this in color
+	};
+
+	var name = {suit, rank, color}
+	makeCard.cardName = function(this.name) {
+		return this in name
+	};
+
+	makeCard.isCard = function(thing) {
+		if card == isCard {
+			return true;
+			}
+		if card !=== isCard {
+			return false;
+		}
+	};
+
+	makeCard.fullSet = [suit, cardName, color, rank];
+
+
+2.
+	var deque = {
+		top: makeDeque.top,
+		bottom: makeDeque.bottom,
+		pop: makeDeque.pop,
+		push: makeDeque.push,
+		shift: makeDeque.shift,
+		unshift: makeDeque.unshift,
+		cut: makeDeque.cut,
+		map: makeDeque.map,
+		sort: makeDeque.sort,
+	};
+
+
+	fuction makeDeque(value) = {
+		return {
+			value.thing
+		}
+	};
+ 
+
+	makeDeque.top = function() {
+
+		return this.arr[i<1]top;
+			
+	};
+
+	makeDeque.bottom = function(thing) {
+		return bottom.thing;
+
+	};
+
+	makeDeque.pop = function(thing) {
+		return this.pop;
+	};
+
+	makeDeque.push = function() {
+		return this.["push"];
+	};
+
+	makeDeque.shift = function() {
+		return this.arr[shift];
+	};
+
+	makeDeque.unshift = function() {
+		return this.
+	};
+
+	makeDeque.cut = function() {
+		this.cut(deque);
+	};
+
+	makeDeque.map = function(convertValFn) {
+		return map.this
+	};
+
+	makeDeque.sort = function(compareValsFn) {
+		return arr[value];
+	};
+
+	//or 
+
+	for 
+
+	function makeDeque(value) {
+	}
+
+
+
+
+
+
+
